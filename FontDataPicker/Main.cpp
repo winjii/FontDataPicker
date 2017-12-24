3c84cce40e6568f58d9ede1dae05448928011536
うぃんじー <wing3196@gmail.com>
2017-12-24 18:39:24 +0900

    FreeTypeの動作確認

--- FontDataPicker/Main.cpp
+++ FontDataPicker/Main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <ft2build.h>
+#include FT_FREETYPE_H
 
 int main() {
-	
+	FT_Library lib;
+	int error = FT_Init_FreeType(&lib);
+	std::cout << error << std::endl;
+
+	FT_Face face;
+	error = FT_New_Face(lib, "SourceHanSerif-Regular.otf", 0, &face);
+	std::cout << error << std::endl;
+
+	return 0;
 }
