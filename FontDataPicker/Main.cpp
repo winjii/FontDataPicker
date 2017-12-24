486fb5874118026691b3cb95872a27b8dad96aab
うぃんじー <wing3196@gmail.com>
2017-12-24 21:54:00 +0900

    FreeTypeのFT_OpenType_Validate()が使えるようにした

--- FontDataPicker/Main.cpp
+++ FontDataPicker/Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <ft2build.h>
 #include FT_FREETYPE_H
+#include FT_OPENTYPE_VALIDATE_H
 
 int main() {
 	FT_Library lib;
@@ -11,5 +12,8 @@ int main() {
 	error = FT_New_Face(lib, "SourceHanSerif-Regular.otf", 0, &face);
 	std::cout << error << std::endl;
 
+	FT_Bytes baseTable, gdefTable, gposTable, gsubTable, jstfTable;
+	error = FT_OpenType_Validate(face, FT_VALIDATE_GSUB, &baseTable, &gdefTable, &gposTable, &gsubTable, &jstfTable);
+	std::cout << error << std::endl;
 	return 0;
 }
